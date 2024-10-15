104e61df1a4006dabace27689ae83d858c78198d
PabloDelgadoSoto <pablodelsot@gmail.com>
2024-10-15 18:59:37 +0200

    esconder codigo

--- katas.js
+++ katas.js
@@ -2,6 +2,13 @@ window.onload = function () {
   console.log();
 }
 
-function disemvowel(str) {
-  return str.replace(/[aeiou]/gi, "");
+function maskify(cc) {
+  for(let i = 0; i < cc.length-4; i++){
+    cc = cc.replace(cc[i], '#');
+  }
+  return cc;
 }
+
+// function disemvowel(str) {
+//   return str.replace(/[aeiou]/gi, "");
+// }
