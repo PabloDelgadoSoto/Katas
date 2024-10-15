812739c2c787ac0bde1bd9975de0e280b329b6f2
PabloDelgadoSoto <pablodelsot@gmail.com>
2024-10-15 19:01:50 +0200

    apariciones impar

--- katas.js
+++ katas.js
@@ -2,13 +2,36 @@ window.onload = function () {
   console.log();
 }
 
-function maskify(cc) {
-  for(let i = 0; i < cc.length-4; i++){
-    cc = cc.replace(cc[i], '#');
+function findOdd(A) {
+  // find what element from given array appears an odd number of times
+  let map = new Map();
+  let sol= 0;
+  for(let i = 0; i < A.length; i++){
+    if(map.get(A[i])){
+      map.set(A[i], map.get(A[i])+1);
+      continue;
+    }
+    map.set(A[i], 1);
   }
-  return cc;
+  for(const [key,value] of map){
+    if(value%2==1){
+      sol=key;
+    }
+  }
+  return sol;
 }
+//solution from commetns that i found clever and want to save
+//const findOdd = (xs) => xs.reduce((a, b) => a ^ b);
+
+// function maskify(cc) {
+//   // mask code but last 4 digits
+//   for(let i = 0; i < cc.length-4; i++){
+//     cc = cc.replace(cc[i], '#');
+//   }
+//   return cc;
+// }
 
 // function disemvowel(str) {
+// // eliminate vowels from string
 //   return str.replace(/[aeiou]/gi, "");
 // }
