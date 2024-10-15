a29a6b8071d7fa657dc78ece1fac3834c0c6acd8
PabloDelgadoSoto <pablodelsot@gmail.com>
2024-10-15 19:03:17 +0200

    proxima raiz cuadrada

--- katas.js
+++ katas.js
@@ -2,26 +2,35 @@ window.onload = function () {
   console.log();
 }
 
-function findOdd(A) {
-  // find what element from given array appears an odd number of times
-  let map = new Map();
-  let sol= 0;
-  for(let i = 0; i < A.length; i++){
-    if(map.get(A[i])){
-      map.set(A[i], map.get(A[i])+1);
-      continue;
-    }
-    map.set(A[i], 1);
+function findNextSquare(sq) {
+  // Return the next square if sq is a perfect square, -1 otherwise
+  let resultado = Math.sqrt(sq);
+  if(resultado%1 == 0){
+    return (resultado+1)**2;
   }
-  for(const [key,value] of map){
-    if(value%2==1){
-      sol=key;
-    }
-  }
-  return sol;
+  return -1;
 }
-//solution from commetns that i found clever and want to save
-//const findOdd = (xs) => xs.reduce((a, b) => a ^ b);
+
+// function findOdd(A) {
+//   // find what element from given array appears an odd number of times
+//   let map = new Map();
+//   let sol= 0;
+//   for(let i = 0; i < A.length; i++){
+//     if(map.get(A[i])){
+//       map.set(A[i], map.get(A[i])+1);
+//       continue;
+//     }
+//     map.set(A[i], 1);
+//   }
+//   for(const [key,value] of map){
+//     if(value%2==1){
+//       sol=key;
+//     }
+//   }
+//   return sol;
+// }
+// //solution from commetns that i found clever and want to save
+// //const findOdd = (xs) => xs.reduce((a, b) => a ^ b);
 
 // function maskify(cc) {
 //   // mask code but last 4 digits
