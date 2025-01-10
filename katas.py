b0772e7da48e370645c61f75c7459a62d8162f0f
PabloDelgadoSoto <pablodelsot@gmail.com>
2025-01-10 20:40:58 +0100

    suma de digitos hasta 1

--- katas.py
+++ katas.py
@@ -1,7 +1,17 @@
-def solution(number):
-#multiples of 3 and 5 below number
+def digital_root(n):
+#sum of digits of a number, repeat until the result is only 1 digit
+    num = str(n)
     sol = 0
-    for num in range(number):
-        if num%3==0 or num%5==0:
-            sol+=num
-    return (sol)
+    for i in range(len(num)):
+        sol+=int(num[i])
+    cadena = str(sol)
+    if len(cadena)>1:sol=digital_root(sol)
+    return(sol)
+
+# def solution(number):
+# #multiples of 3 and 5 below number
+#     sol = 0
+#     for num in range(number):
+#         if num%3==0 or num%5==0:
+#             sol+=num
+#     return (sol)
